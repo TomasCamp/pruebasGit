3f7de7080ed98814a3340a4f61c4a16fe65253a7
Tomas Campanella <tomyecamp@gmail.com>
2024-08-28 23:35:18 -0300

    Actualizo el algoritmo de calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,11 +1,12 @@
+import operations
 def calculate(a, b, operator):
     if operator == "+":
-        return a + b
-    elif operator == "+":
-        return a + b
-    elif operator == "+":
-        return a + b
-    elif operator == "+":
-        return a + b
+        return operations.add(a, b)
+    elif operator == "-":
+        return operations.subtract(a, b)
+    elif operator == "*":
+        return operations.multiply(a, b)
+    elif operator == "/":
+        return operations.divide(a, b)
     else:
         raise ValueError(f"Operador no valido: {operator}")
